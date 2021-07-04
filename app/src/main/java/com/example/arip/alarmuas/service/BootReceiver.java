9152296157d344b5be137d58c9b39100fea5357e
adiyogta <adiyogta@gmail.com>
2021-07-04 21:17:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/arip/alarmuas/service/BootReceiver.java
@@ -0,0 +1,33 @@
+package com.example.arip.alarmuas.service;
+
+import android.app.AlarmManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.arip.alarmuas.data.DatabaseHelper;
+import com.example.arip.alarmuas.model.Alarm;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import static android.content.Intent.ACTION_BOOT_COMPLETED;
+import static com.example.arip.alarmuas.service.AlarmReceiver.setReminderAlarms;
+
+/**
+ * Re-schedules all stored alarms. This is necessary as {@link AlarmManager} does not persist alarms
+ * between reboots.
+ */
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Executors.newSingleThreadExecutor().execute(() -> {
+                final List<Alarm> alarms = DatabaseHelper.getInstance(context).getAlarms();
+                setReminderAlarms(context, alarms);
+            });
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/arip/alarmuas/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.arip.alarmuas.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.arip.alarmuas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
